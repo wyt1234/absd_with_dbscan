7a1661d3465fd2fe96696c10fedd0f504dc56260
wyt1234 <976582772@qq.com>
2022-05-11 21:25:48 +0800

    聚类散点

--- demo1.py
+++ demo1.py
@@ -145,6 +145,61 @@ def draw_3d_scatters(df_list: List[Union[DataFrame]]):
     plt.show()
 
 
+# 聚类中加入航向和航速，限定一个范围，并用颜色标出不同聚类点
+def draw_3d_scatters2(df_list: List[Union[DataFrame]], random_rate=1.0):
+    # 将点云concat
+    df = pd.concat(df_list)
+    df = df.dropna()
+    df = df.sample(frac=random_rate)  # 按比例随机采样
+    X = df[['lat', 'lon', 'geoaltitude', 'velocity', 'heading']].to_numpy()
+    # todo 聚类时加入航向和航速特征并归一化
+    db = DBSCAN(eps=0.3, min_samples=10).fit(X)
+    core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
+    core_samples_mask[db.core_sample_indices_] = True
+    labels = db.labels_
+
+    # Number of clusters in labels, ignoring noise if present.
+    n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+    n_noise_ = list(labels).count(-1)
+
+    print("Silhouette Coefficient: %0.3f" % metrics.silhouette_score(X, labels))
+    X = np.column_stack([X, labels])
+    X_nagetive = X[X[:, 5] == -1]  # 无效样本
+    X = X[X[:, 5] != -1]
+    # 类的数量 -> 取出排名前N个类
+    ord_dic_list = [{'cluster_index': i, 'amt': len(X[X[:, 5] == i])} for i in range(n_clusters_)]
+    ord_dic_list = sorted(ord_dic_list, key=lambda x: x['amt'], reverse=True)
+    topk_cluster = [ord_dic_list[i]['cluster_index'] for i in range(5)]  # todo hyperparam
+    # 开始绘制散点图
+    # mandarin support
+    plt.rcParams['font.sans-serif'] = ['SimHei']
+    plt.rcParams['axes.unicode_minus'] = False
+    expand = 2
+    fig = plt.figure()
+    ax = plt.figure(figsize=[6.4 * expand, 4.8 * expand], dpi=100.0 / expand).add_subplot(projection='3d')
+    # 绘制没有被分类的点云为灰色
+    temp_nd = X_nagetive
+    xs = temp_nd[:, 0]
+    ys = temp_nd[:, 1]
+    zs = temp_nd[:, 2]
+    m = 'o'
+    ax.scatter(xs, ys, zs, marker=m, s=0.05, c='#7f7f7f', alpha=0.25)
+    # 绘制有分类的点云
+    for cluster_index in topk_cluster:
+        temp_nd = X[X[:, 5] == cluster_index]
+        xs = temp_nd[:, 0]
+        ys = temp_nd[:, 1]
+        zs = temp_nd[:, 2]
+        m = 'o'
+        ax.scatter(xs, ys, zs, marker=m, s=100.0)
+    ax.legend()
+    ax.set(xlim3d=(10, 60), xlabel='lat')
+    ax.set(ylim3d=(-150, 100), ylabel='lon')
+    ax.set(zlim3d=(0, 12000), zlabel='geoaltitude')
+    ax.set_title("航迹点云聚类")
+    plt.show()
+
+
 if __name__ == '__main__':
     ############# todo 如果是第一次跑放开下面这行方法注释 ###########
     # split_files(150)  # 切分文件到xlsx目录
@@ -153,4 +208,4 @@ if __name__ == '__main__':
     # df_list = read_csvs('csv', 'D:\Repo\dbscan_absd\爬取数据\M-C17运输-AE1450')
     # draw_3d_lines(df_list)  # 折现航迹
     draw_line_scatters(df_list, random_rate=0.15)  # 散点航迹
-    # draw_3d_scatters(df_list) # 聚类散点
+    draw_3d_scatters2(df_list, random_rate=0.012)  # 聚类散点
